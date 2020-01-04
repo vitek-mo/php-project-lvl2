ca16aec8fe68022cf2380a44ae2815c267af0e34
Viktor Moisseyenko <vitek.mo@gmail.com>
2020-01-04 23:06:38 +0600

    little refactoring in Parsers

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,16 +6,14 @@ use Symfony\Component\Yaml\Yaml;
 
 function parse($contents, $format)
 {
+    [$content1, $content2] = $contents;
     if ($format === 'json') {
-        [$json1, $json2] = $contents;
-        $parsed1 = json_decode($json1, true);
-        $parsed2 = json_decode($json2, true);
-        return [$parsed1, $parsed2];
+        $parsed1 = json_decode($content1, true);
+        $parsed2 = json_decode($content2, true);
     }
     if ($format === 'yml') {
-        [$yml1, $yml2] = $contents;
-        $parsed1 = Yaml::parse($yml1);
-        $parsed2 = Yaml::parse($yml2);
-        return [$parsed1, $parsed2];
+        $parsed1 = Yaml::parse($content1);
+        $parsed2 = Yaml::parse($content2);
     }
+    return [$parsed1, $parsed2];
 }
